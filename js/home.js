f28c2d931ff72e3714ab3425ece67728753e52ff
AymericM <aymeric.moehn@gmail.com>
2018-02-13 15:48:45 +0100

    homepage done (manque le mobile)

--- js/home.js
+++ js/home.js
@@ -1,6 +1,10 @@
+const page = document.getElementsByTagName('html')[0]
+const header = document.getElementsByClassName('header')[0]
+const navbar = document.getElementsByClassName('navbarContent')[0]
 const btnLeft = document.getElementById('btnLeft')
 const btnRight = document.getElementById('btnRight')
 let imgs = document.getElementsByClassName('carousel_img')
+let txt = document.getElementById('carouselTxt')
 let index = 0
 
 document.getElementById('carousel').style.width = `${100*imgs.length}vw`
@@ -19,6 +23,18 @@ btnLeft.addEventListener('click', () => {
         index -= 100
         for (let i = 0; i < imgs.length; i++) {
             imgs[i].style.transform = `translateX(-${index}%)`
+        }
+    }
+})
+
+window.addEventListener('scroll', () => {
+    if (page.scrollTop > header.clientHeight) {
+        if (!navbar.classList.contains('big')) {
+            navbar.classList.add('big')
+        }
+    } else {
+        if (navbar.classList.contains('big')) {
+            navbar.classList.remove('big')
         }
     }
 })
