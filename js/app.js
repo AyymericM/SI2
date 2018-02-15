6c086ca1390b0827a5f62c65249444e272df74f2
AymericM <aymeric.moehn@gmail.com>
2018-02-15 15:30:44 +0100

    FINISHED

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const menuBtn = document.getElementsByClassName('mobileMenu')[0]
 const menuContent = document.getElementsByClassName('navbarContent')[0]
+const cartCount = document.getElementById('cartCount')
 
 menuBtn.addEventListener('click', () => {
     if (menuContent.classList.contains('mobileMenuOpen')) {
@@ -7,4 +8,17 @@ menuBtn.addEventListener('click', () => {
     } else {
         menuContent.classList.add('mobileMenuOpen')
     }
+});
+
+(() => {
+    if (sessionStorage.getItem('cartCount')) {
+        cartCount.innerText = sessionStorage.getItem('cartCount')
+    } else {
+        cartCount.innerText = '0'
+    }
+})()
+
+cartCount.addEventListener('click', () => {
+    sessionStorage.setItem('cartCount', 0)
+    cartCount.innerText = "0"
 })
--- js/product.js
+++ js/product.js
@@ -1,5 +1,6 @@
 const btns = document.getElementsByClassName('productTabLink')
 const content = document.getElementsByClassName('productDetailsContent')
+const addBtn = document.getElementsByClassName('addBtn')[0]
 
 for (let i = 0; i < btns.length; i++) {
     btns[i].addEventListener('click', (e) => {
@@ -11,3 +12,14 @@ for (let i = 0; i < btns.length; i++) {
         content[i].classList.add('activeDetails')
     })
 }
+
+addBtn.addEventListener('click', () => {
+    if (sessionStorage.getItem('cartCount')) {
+        let newCount = parseInt(sessionStorage.getItem('cartCount')) + 1
+        sessionStorage.setItem('cartCount', newCount)
+        cartCount.innerText = newCount
+    } else {
+        sessionStorage.setItem('cartCount', 1)
+        cartCount.innerText = '1'
+    }
+})
